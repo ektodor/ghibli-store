7f2ac9423d30f9543df344afb1659d5d49b20f32
Tippy Wang <ektodorwang@gmail.com>
2024-03-18 22:34:32 +0800

    Updates

--- assets/AdminArticlesView-cCXTLd9N.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-nZ-bWd1c.js";const t={};function r(c,n,s,i,o,_){return" AdminArticleView "}const p=e(t,[["render",r]]);export{p as default};
---
+++ assets/utils-sTL9UzG1.js
@@ -0,0 +1,1 @@
+const o=t=>new Date(Math.floor(t).toString().length===10?t*1e3:t).toLocaleString("zh-TW",{hour12:!1}),n=t=>new Date(t).getTime()/1e3;export{n as a,o as t};
